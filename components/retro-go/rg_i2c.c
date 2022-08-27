d9088d92c5513326da8f094c0316adec8be77bf8
Alex Duchesne <alex@netvps.ca>
2022-08-26 20:41:01 -0400

    rg_i2c: Adding gpio extender driver
    Still a WIP but I think having gpio extender support in the core makes sense given the growing number of devices using one.

--- components/retro-go/rg_i2c.c
+++ components/retro-go/rg_i2c.c
@@ -1,24 +1,24 @@
-#include <esp_system.h>
+#include <freertos/FreeRTOS.h>
+#include <esp_err.h>
 #include "rg_system.h"
 #include "rg_i2c.h"
 
 #if defined(RG_GPIO_I2C_SDA) && defined(RG_GPIO_I2C_SCL)
-    #include <freertos/FreeRTOS.h>
     #include <driver/i2c.h>
     #define USE_I2C_DRIVER
 #endif
 
 #define TRY(x) if ((err = (x)) != ESP_OK) { goto fail; }
 
-static enum {
-    INIT_PENDING,
-    INIT_FAILED,
-    INIT_OK
-} i2cStatus = INIT_PENDING;
+static bool i2c_initialized = false;
+static rg_i2c_gpio_t gpio_extender = RG_I2C_NONE;
+static uint8_t gpio_extender_addr = 0;
 
 
 bool rg_i2c_init(void)
 {
+    if (i2c_initialized)
+        return true;
 #ifdef USE_I2C_DRIVER
     const i2c_config_t i2c_config = {
         .mode = I2C_MODE_MASTER,
@@ -33,42 +33,32 @@ bool rg_i2c_init(void)
     TRY(i2c_param_config(I2C_NUM_0, &i2c_config));
     TRY(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
     RG_LOGI("I2C driver ready (SDA:%d SCL:%d).\n", i2c_config.sda_io_num, i2c_config.scl_io_num);
-    i2cStatus = INIT_OK;
+    i2c_initialized = true;
     return true;
 fail:
     RG_LOGE("Failed to initialize I2C driver. err=0x%x\n", err);
 #else
     RG_LOGE("I2C driver is not available on this device.\n");
 #endif
-    i2cStatus = INIT_FAILED;
+    i2c_initialized = false;
     return false;
 }
 
 bool rg_i2c_deinit(void)
 {
 #ifdef USE_I2C_DRIVER
-    if (i2cStatus != INIT_PENDING)
-    {
-        if (i2c_driver_delete(I2C_NUM_0) == ESP_OK)
-            RG_LOGI("I2C driver terminated.\n");
-    }
+    if (i2c_initialized && i2c_driver_delete(I2C_NUM_0) == ESP_OK)
+        RG_LOGI("I2C driver terminated.\n");
 #endif
-    i2cStatus = INIT_PENDING;
+    i2c_initialized = false;
     return true;
-}
-
-bool rg_i2c_available(void)
-{
-    if (i2cStatus == INIT_PENDING)
-        return rg_i2c_init();
-    return i2cStatus == INIT_OK;
 }
 
 bool rg_i2c_read(uint8_t addr, int reg, void *read_data, size_t read_len)
 {
     esp_err_t err = ESP_FAIL;
 
-    if (!rg_i2c_available())
+    if (!i2c_initialized)
         goto fail;
 
 #ifdef USE_I2C_DRIVER
@@ -89,7 +79,7 @@ bool rg_i2c_read(uint8_t addr, int reg, void *read_data, size_t read_len)
 #endif
 
 fail:
-    RG_LOGE("Read from 0x%02x failed. reg=%d, err=0x%x\n", addr, reg, err);
+    RG_LOGE("Read from 0x%02x failed. reg=%d, err=0x%x\n", addr, reg, err, i2c_initialized);
     return false;
 }
 
@@ -97,7 +87,7 @@ bool rg_i2c_write(uint8_t addr, int reg, const void *write_data, size_t write_le
 {
     esp_err_t err = ESP_FAIL;
 
-    if (!rg_i2c_available())
+    if (!i2c_initialized)
         goto fail;
 
 #ifdef USE_I2C_DRIVER
@@ -116,7 +106,7 @@ bool rg_i2c_write(uint8_t addr, int reg, const void *write_data, size_t write_le
 #endif
 
 fail:
-    RG_LOGE("Write to 0x%02x failed. reg=%d, err=0x%x\n", addr, reg, err);
+    RG_LOGE("Write to 0x%02x failed. reg=%d, err=0x%x, init=%d\n", addr, reg, err, i2c_initialized);
     return false;
 }
 
@@ -129,4 +119,77 @@ uint8_t rg_i2c_read_byte(uint8_t addr, uint8_t reg)
 {
     uint8_t value = 0;
     return rg_i2c_read(addr, reg, &value, 1) ? value : 0;
+}
+
+bool rg_i2c_gpio_init(rg_i2c_gpio_t device_type, uint8_t addr)
+{
+    RG_ASSERT(gpio_extender == RG_I2C_NONE, "GPIO extender already initialized!");
+
+    if (!i2c_initialized && !rg_i2c_init())
+        goto fail;
+
+    // if (device_type == RG_I2C_AW9523)
+    {
+        gpio_extender = RG_I2C_AW9523;
+        gpio_extender_addr = addr ?: AW9523_DEFAULT_ADDR;
+
+        rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_SOFTRESET, 0);
+        vTaskDelay(pdMS_TO_TICKS(10));
+
+        uint8_t id = rg_i2c_read_byte(gpio_extender_addr, AW9523_REG_CHIPID);
+        RG_LOGI("AW9523 ID code 0x%x found\n", id);
+        assert(id == 0x23);
+
+        rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_CONFIG0, 0xFF);
+        rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_CONFIG0+1, 0xFF);
+        rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_LEDMODE, 0xFF);
+        rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_LEDMODE+1, 0xFF);
+        rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_GCR, 1<<4);
+        return true;
+    }
+
+fail:
+    gpio_extender = RG_I2C_NONE;
+    gpio_extender_addr = 0;
+    return false;
+}
+
+bool rg_i2c_gpio_deinit(void)
+{
+    gpio_extender = RG_I2C_NONE;
+    gpio_extender_addr = 0;
+    return true;
+}
+
+bool rg_i2c_gpio_setup_port(int port, uint16_t config)
+{
+    return rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_CONFIG0, config & 0xFF)
+        && rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_CONFIG0+1, config >> 8);
+}
+
+uint16_t rg_i2c_gpio_read_port(int port)
+{
+    return (rg_i2c_read_byte(gpio_extender_addr, AW9523_REG_INPUT0+1) << 8)
+         | (rg_i2c_read_byte(gpio_extender_addr, AW9523_REG_INPUT0));
+}
+
+bool rg_i2c_gpio_write_port(int port, uint16_t value)
+{
+    return rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_OUTPUT0, value & 0xFF)
+        && rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_OUTPUT0+1, value >> 8);
+}
+
+int rg_i2c_gpio_get_level(int pin)
+{
+    return (rg_i2c_gpio_read_port(0) >> pin) & 1;
+}
+
+bool rg_i2c_gpio_set_level(int pin, int level)
+{
+    uint16_t pins = (rg_i2c_read_byte(gpio_extender_addr, AW9523_REG_OUTPUT0+1) << 8)
+                  | (rg_i2c_read_byte(gpio_extender_addr, AW9523_REG_OUTPUT0));
+    pins &= ~(1UL << pin);
+    pins |= (level & 1) << pin;
+    return rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_OUTPUT0, pins & 0xFF)
+        && rg_i2c_write_byte(gpio_extender_addr, AW9523_REG_OUTPUT0+1, pins >> 8);
 }
--- components/retro-go/rg_i2c.h
+++ components/retro-go/rg_i2c.h
@@ -4,11 +4,37 @@
 #include <stdint.h>
 #include <stddef.h>
 
+typedef enum
+{
+    RG_I2C_NONE = 0,
+    RG_I2C_AW9523,
+    RG_I2C_MCP23008,
+    RG_I2C_MCP23017,
+} rg_i2c_gpio_t;
+
 bool rg_i2c_init(void);
 bool rg_i2c_deinit(void);
-bool rg_i2c_available(void);
 bool rg_i2c_read(uint8_t addr, int reg, void *read_data, size_t read_len);
 bool rg_i2c_write(uint8_t addr, int reg, const void *write_data, size_t write_len);
-bool rg_i2c_write_byte(uint8_t addr, uint8_t reg, uint8_t value);
 uint8_t rg_i2c_read_byte(uint8_t addr, uint8_t reg);
+bool rg_i2c_write_byte(uint8_t addr, uint8_t reg, uint8_t value);
 
+// GPIO extender
+bool rg_i2c_gpio_init(rg_i2c_gpio_t device_type, uint8_t addr);
+bool rg_i2c_gpio_deinit(void);
+bool rg_i2c_gpio_setup_port(int port, uint16_t config);
+bool rg_i2c_gpio_set_level(int pin, int level);
+int  rg_i2c_gpio_get_level(int pin);
+uint16_t rg_i2c_gpio_read_port(int port);
+bool rg_i2c_gpio_write_port(int port, uint16_t value);
+
+
+#define AW9523_DEFAULT_ADDR 0x58
+#define AW9523_REG_CHIPID 0x10     ///< Register for hardcode chip ID
+#define AW9523_REG_SOFTRESET 0x7F  ///< Register for soft resetting
+#define AW9523_REG_INPUT0 0x00     ///< Register for reading input values
+#define AW9523_REG_OUTPUT0 0x02    ///< Register for writing output values
+#define AW9523_REG_CONFIG0 0x04    ///< Register for configuring direction
+#define AW9523_REG_INTENABLE0 0x06 ///< Register for enabling interrupt
+#define AW9523_REG_GCR 0x11        ///< Register for general configuration
+#define AW9523_REG_LEDMODE 0x12    ///< Register for configuring const current
